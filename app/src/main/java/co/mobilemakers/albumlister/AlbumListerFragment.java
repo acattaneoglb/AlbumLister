d3ac3fdbaf6f94789594af7865b79bad04f6a57b
Ariel Cattáneo <ariel.cattaneo@globant.com>
2015-02-13 12:04:58 -0300

    Initial commit.
    The project is created

---
+++ app/src/main/java/co/mobilemakers/albumlister/AlbumListerFragment.java
@@ -0,0 +1,23 @@
+package co.mobilemakers.albumlister;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class AlbumListerFragment extends Fragment {
+
+    public AlbumListerFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_album_lister, container, false);
+        return rootView;
+    }
+}
